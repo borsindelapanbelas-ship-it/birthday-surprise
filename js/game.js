74b874c2708f6dbec19848926e74231d8abc2cd7
borsindelapanbelas-ship-it <borsindelapanbelas@gmail.com>
2026-01-17 21:51:40 +0900

    Update game.js

--- js/game.js
+++ js/game.js
@@ -24,10 +24,10 @@ playerImgs.right.src = "assets/images/right.png";
 /* ===== PLAYER ===== */
 const SCALE = 0.15;
 const player = {
-  x: 85,          // PAS DI TENGAH JALUR MASUK
-  y: 435,         // MASUK KE DALAM MAZE
+  x: 65,      // MASUK KE LUBANG ATAS KIRI
+  y: 55,      // PAS DI JALURNYA
   speed: 3,
-  dir: "back",
+  dir: "right",
   w: 0,
   h: 0
 };
