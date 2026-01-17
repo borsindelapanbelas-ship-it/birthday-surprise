022d51cadf5bd12ecc9234395e3ef2d1d15f9b97
borsindelapanbelas-ship-it <borsindelapanbelas@gmail.com>
2026-01-17 21:53:13 +0900

    Update game.js

--- js/game.js
+++ js/game.js
@@ -24,7 +24,7 @@ playerImgs.right.src = "assets/images/right.png";
 /* ===== PLAYER ===== */
 const SCALE = 0.15;
 const player = {
-  x: 65,      // MASUK KE LUBANG ATAS KIRI
+  x: 60,      // MASUK KE LUBANG ATAS KIRI
   y: 55,      // PAS DI JALURNYA
   speed: 3,
   dir: "right",
