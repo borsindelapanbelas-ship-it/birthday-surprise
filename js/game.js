4679a88e11f07aebb1b9f82d5198e7a09882bc0d
sansiiiin <borsindelapanbelas@gmail.com>
2026-02-18 21:34:22 +0900

    Update game.js

--- js/game.js
+++ js/game.js
@@ -534,7 +534,7 @@ Di hari yang spesial ini, kiranya kasih dan penyertaan Tuhan selalu memenuhi set
 menjadi kakak yang menguatkan dan membawa banyak berkat bagi orang-orang di sekitar.
 "TUHAN memberkati engkau dan melindungi engkau; TUHAN menyinari engkau dengan wajah-Nya dan memberi engkau kasih karunia; 
 TUHAN menghadapkan wajah-Nya kepadamu dan memberi engkau damai sejahtera." — Bilangan 6:24-26
-Tetap jadi terang di mana pun Kakak berada, dan terus berjalan dalam rencana TUhan yang indah. Tuhan Yesus memberkati selalu ✨`,
+Tetap jadi terang di mana pun Kakak berada, dan terus berjalan dalam rencana Tuhan yang indah. Tuhan Yesus memberkati selalu ✨`,
 
 `From Eto
 
@@ -571,6 +571,131 @@ Happy birthday kak Virgin dalam nama Tuhan Yesus kakak diberkati Tuhan dan panja
 Hi kakk Virgin, Blessed birthday 🥳🥳🥳. Thank you for being good leader terutama buat aku. Declaring 29:11 for u
 [11] Sebab Aku ini mengetahui rancangan-rancangan apa yang ada pada-Ku mengenai kamu, demikianlah firman TUHAN,
 yaitu rancangan damai sejahtera dan bukan rancangan kecelakaan, untuk memberikan kepadamu hari depan yang penuh harapan.`,
+
+`From Jeremiah
+
+Blessed birthday Kak Virgin 🥳🥳🥳 May the Lord's blessing always be upon you to protect, strengthen, and guides
+you through every challenges. Wish you all the best!!!`,
+
+`From Jeremy JAC
+
+happy bday ci virgin, I believe in the name of Jesus that you are blessed in this special day, hope u do better in
+ur jobs and in every aspect of ur live, GBU`,
+
+`From Jessica
+
+Selamat ulang tahun Kaa Virginnn 💗💗!! Semoga di hari yang sangat spesial inii, kakaa mendapatkan banyak rezeki dan 
+panjang umur (≧▽≦)! Aku berharap kakak mendapatkan kebahagiaan, keharmonisan dalam kebersamaan dan perubahan baik 
+dalam hidup kakaa 😊`,
+
+`From Jessica Kimberly
+
+Happy birthday kak Virgin! 🥳🥳 Terima kasih udah bimbing kita semua dalam Yesus. Kak Virgin selalu sabar mendukung kita
+lebih dekat dengan Tuhan dan mengajar kita tentang kasihNya. Semoga kakak panjang umur, sehat, dan sukses selalu. God bless youu 🥳🎉`,
+
+`From Joa
+
+Happy bday kak Virgin, sukses slalu, God bless you`,
+
+`From Jonathan Lim
+
+Selamat ulang tahun kak Virgin panjang umur semoga sehat selalu semoga semua harapam dan doa dikabulkan di tahun ini, amin`,
+
+`From Jovi
+
+Happy Birthday Ka Virgin`,
+
+`From Kayara
+
+Blessed birthday Ka Virgin!! May all your dreams come true and God Bless!!`,
+
+`From Kayla
+
+Hai kaa, aku Kaylaa. Happiest birthday to youu 🫶 semoga sehat sehat terus yaa. Mungkin kaka blom kenal aku karna baru join
+pas natal hehe. But glad to know u kaa. God bless uu 🫶🫶`,
+
+`From Kinar
+
+Blessed birthday ci Virgin ❤️🩷❤️🩷❤️🩷 God bless you may this birthday be as amazing as you are!!
+I hope this year treats you super sososo well and may your relationship with Jesus just keep getting better. I pray you
+always rely on HIm and trust that He has wonderful plans for you on your birthday and always 🫶🫶
+You remind me so much of a proverbs 31 woman so i really hope you're always surrounded by loved one :o love you ci`,
+
+`From Lyonell JAC
+
+Hii kak Virgin happy birthday, may God always bless you`,
+
+`From Michelle
+
+Happy Birthday Kak Virgin!
+May God bless you with abundant health, joy, and peace in your birthday. Thank you for being such a kind, caring, and inspiring
+person in our CG. May God continue to guide your steps, strengthen your faith, and fill your life with His endless love and grace.
+Stay blessed always Kak Virgin 🙏✨`,
+
+`From Miguel
+
+Hi kak Virgin, this is from migel!, i wanna wish you a happy Blessed birthday!!!, Hope today tomorrow and forever you will
+have a lot og joy, and always be healthy with God in Christ!`,
+
+`From Nathan
+
+Selamat ulang tahun and bless birthday Kak Virgin 🥳🥳🥳`,
+
+`From Nikita
+
+Blessed birthday kak Virgin, wish u all the best kak Virgin, all kak Virgin work will be easy by God ya kak 🙇‍♀️🤍`,
+
+`From Oliv
+
+Your big day ka Virgin! Remember that you are loved by the people around you, and above all, you're loved by DaddyGod :D
+He will make all the crooked places straight, and show His glory, even in times when it seems unlikely. ESPECIALLY in those times.`,
+
+`From Oliver
+
+Selamat Ulang tahun kak Virgin!! Semoga hidup kakak berjalan dengan baik, sehat, lancar, menyenangkan, dan keren semoga kakak juga panjang umur dan damai sejahtera, God bless you ka!`,
+
+`From Raphael
+
+Happy Birthday Kak Virgin, God Bless You`,
+
+`From Ryan
+
+happy birthday kak virgin! sehat selalu dan panjang umur ya`,
+
+`From Samuel Bryan 
+
+Blessed Birthday Kak Virgin!!! Panjang umur and sehat selalu, Surely goodness and mercy will follow you for the rest of your life. Praying for more open doors and new opportunities and experiences this year! Have a happy birthday 🥳🥳🥳🥳🔥🔥🔥🔥`,
+
+`From Shalom
+
+Haii kak Virginnn 👋👋
+Happy Birthdayy yaaa 🥳🥳
+Merry Christmas and happy new year juga kak Virginn 🎉
+Thankyou ya kakk udah nemenin Shalom di
+Grace Army dari awal joinn 😊😊❤️
+Semoga di tahun 2026 ini kakak bisa mencapai goals kak Virginn, diberi kelimpahan rezeki, dan semakin dekat kepada Tuhann🤍🤍
+Sekali lagi have a blessed birthday kak
+Virginn, God bless youu 🥳🎂❤️`,
+
+`From Thania
+
+Selamat ulang tahun kak Virginn, kiranya Tuhan Yesus selalu memberkati, memberi kesehatan, sukacita, dan damai sejahtera dalam setiap langkah.`,
+
+`From Theo
+
+Happy Birthday Kak Virgin!Aku mau ngucapin Selamat Ulang Tahun kak. Semoga Kaka selalu di berkati oleh Tuhan God Bless You Kak Virgin sehat selalu!`,
+
+`From Victory JAC
+
+Happy birthday kak Virgin, semoga sehat selalu, diberikan panjang umur dan selalu dalam penyertaan Tuhan Yesus. Terimakasih sudah menjadi CGL yang baik. Dan semoga selalu diberikan kemudahan oleh Tuhan.`,
+
+`From Xander
+
+Happy birthday kak Virgin Tuhan Yesus memberkati 🥳`,
+
+`From Yemima
+
+haiihaiii ka virgin, ini mima that tiffany yemima yang suka tiba tiba jailin ka virgin, ngintilin ka virgin 24/7 di gereja, apalagi pas camp 2024 😹 first of all happy birthday ka virgin, asik ni yaa udah bertambah umur 👀❗️ thank you ya ka for being the best CGL in my heart since that youth revival camp, dream camp 2024! i didn't asked God buat di 2024 kenal ama kaka, but He knows whats best for me and kaka! (love light of the world and greatly blessed 💟) pas aku baru pertama kali camp di Grace Army itu aku ga deket ataupun kenal ama siapa", ka virgin itu salah satu orang yang Tuhan rencanain buat deket ama akuuu 🥲🫶🏻 kaka mau dengerin aku cerita tiap minggu, mau aku ajak ngobrol tiap ketemu alias kaka dengerin yappingan aku, bahkan aku kadang suka tiba-tiba ngchat gara-gara mau cerita aja giru atau nanyain kabar kakaa 🫣! pokoknya semenjak aku dikenalin ke kaka ama Tuhan, aku jadi lebih berani buat dateng ke Grace Army lebih rutin/sering, lebih berani bergaul ama orang-orang di gereja, terus lebih berani buat sharing pas CG depan kingdom friends yang lain tanpa buka hp! sayang banget ama kak virgin 🫠💟 aku berdoa di umur ka virgin yang sekarang, ka virgin makin panjang umur, sehat selalu, makin rajin pelayanan di gereja, pekerjaan kaka selalu lancar and declaring all the good things from God buat kaka, apalagi the things we can't see but we know that He is working on something  in your life and making something enter in your life ka! pokoknya thank you again ya ka, soalnya Tuhan udah bisa buat kaka jd berkat yang cukup amat besar di hidup aku 🔥 can't imagine kalo waktu 2024 ak engga kenal ama kaka.. aku sekarang gimana cobaa 🥹??? overall always be the ka virgin yang aku kenal ya kaa, that kak virgin yang happy ngomongin segala hal yang Tuhan telah lakuin di hidup kaka, kak virgin yang caring banget sama perasaan-perasaan kita, kak virgin yang selalu ingetin kita tentang betapa baiknya Dia ke kitaa 🫂🫂 so yeah thats from me kaa ^^ have a blessed year ya kak virgin my beloved CGL 🙌🏻😆
 
 ];
 
